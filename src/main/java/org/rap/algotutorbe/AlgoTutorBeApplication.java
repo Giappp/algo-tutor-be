7d817ab4dfadf0021ac4e8e30121ba7413f55e50
Rapppuwu <giap8547813@gmail.com>
2026-02-25 21:37:51 +0700

    Initial commit

---
+++ src/main/java/org/rap/algotutorbe/AlgoTutorBeApplication.java
@@ -0,0 +1,13 @@
+package org.rap.algotutorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoTutorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoTutorBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rap/algotutorbe/AlgoTutorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rap.algotutorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoTutorBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
