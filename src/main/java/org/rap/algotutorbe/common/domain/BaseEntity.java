e78b679404c0f1e29aa4057c728a181d4c0b5bb8
Rapppuwu <giap8547813@gmail.com>
2026-03-09 23:03:01 +0700

    feat: add BaseEntity class for JPA auditing and package-info for module definition

---
+++ src/main/java/org/rap/algotutorbe/common/domain/BaseEntity.java
@@ -0,0 +1,27 @@
+package org.rap.algotutorbe.common.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/org/rap/algotutorbe/common/package-info.java
@@ -0,0 +1,6 @@
+@org.springframework.modulith.ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package org.rap.algotutorbe.common;
+
+import org.springframework.modulith.ApplicationModule;
