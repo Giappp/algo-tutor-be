6e6c0140dd0c7325d35d43f6d70695480467f891
Nguyen Van Giap <giapnv@gem-corp.global>
2026-03-16 15:47:10 +0700

    feat: Base class for Api Response and Page response

--- src/main/java/org/rap/algotutorbe/common/api/ApiResponse.java
+++ src/main/java/org/rap/algotutorbe/common/api/ApiResponse.java
@@ -1,4 +1,15 @@
 package org.rap.algotutorbe.common.api;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private T data;
+    private T messages;
+    private String code;
+    private Boolean success;
 }
--- src/main/java/org/rap/algotutorbe/common/api/PageResponse.java
+++ src/main/java/org/rap/algotutorbe/common/api/PageResponse.java
@@ -1,4 +1,16 @@
 package org.rap.algotutorbe.common.api;
 
-public class PageResponse {
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@Data
+public class PageResponse<T> {
+    List<T> data;
+    Integer pageSize;
+    Integer totalPages;
+    Long totalElements;
+    Integer currentPage;
 }
