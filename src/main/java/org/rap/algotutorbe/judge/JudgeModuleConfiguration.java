cb137f4edf7282ac23768b1bb442bad9da18bb85
Nguyen Van Giap <giapnv@gem-corp.global>
2026-03-17 16:04:05 +0700

    feat: config judge module

---
+++ src/main/java/org/rap/algotutorbe/judge/JudgeModuleConfiguration.java
@@ -0,0 +1,9 @@
+package org.rap.algotutorbe.judge;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.rap.algotutorbe.judge")
+public class JudgeModuleConfiguration {
+}
